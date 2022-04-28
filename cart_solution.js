bd723b7bfe831b711bbad1c614bf6da85210d2cc
AS2502 <akash.33sharma@gmail.com>
2022-04-28 22:22:49 +0530

    Wholesale problem completed

--- cart_solution.js
+++ cart_solution.js
@@ -5,10 +5,10 @@
 
 // Break-up
 // Compute final price for an item
-  // Get unit price
-  // Compute item discount
-  // Compute tax on item
-  // Compute final price
+// Get unit price
+// Compute item discount
+// Compute tax on item
+// Compute final price
 
 // Compute the price for a lineItem. Note: Every purchase will have a lineItem in the bill.
 
@@ -16,87 +16,84 @@
 
 /* Data */
 var rates = {
-    Carrot: 10,
-    Apple: 200,
-    Guava: 50,
+  Carrot: 10,
+  Apple: 200,
+  Guava: 50,
+};
+
+var discounts = {
+  // values are in percentages.
+  Apple: 10,
+};
+
+var taxes = {
+  // values are in percentages.
+  Carrot: 5,
+  Guava: 10,
+};
+
+var purchases = [
+  {
+    item: 'Carrot',
+    units: 20,
+  },
+  {
+    item: 'Apple',
+    units: 2,
+  },
+  {
+    item: 'Guava',
+    units: 1,
+  },
+];
+
+/* Functions */
+const getDiscountPercent = (productName) => {
+  let discount = discounts[productName];
+  let discountPercent = (discount > 0) ? (discount / 100) : 0;
+  return discountPercent;
+};
+
+const getTaxPercent = (productName) => {
+  let tax = taxes[productName];
+  let taxPercent = (tax > 0) ? (tax / 100) : 0;
+  return taxPercent;
+};
+
+const getUnitPrice = (itemName) => {
+  let unitPrice = rates[itemName];
+  let discountPercent = getDiscountPercent(itemName);
+  let itemDiscountedPrice = unitPrice * discountPercent;
+  let discountedPrice = unitPrice - itemDiscountedPrice;
+  let taxPercent = getTaxPercent(itemName);
+  let taxPrice = discountedPrice * taxPercent;
+  let finalPrice = discountedPrice + taxPrice;
+  return finalPrice;
+};
+
+const getLineItemPrice = (lineItem) => {
+  let itemName = lineItem['item'];
+  let unit = lineItem['units'];
+  let unitPrice = getUnitPrice(itemName);
+  let totalUnitsPrice = unitPrice * unit;
+  let itemPrice = {
+    'price': totalUnitsPrice,
+    'item': itemName,
   };
-  
-  var discounts = {
-    // values are in percentages.
-    Apple: 10,
-  };
-  
-  var taxes = {
-    // values are in percentages.
-    Carrot: 5,
-    Guava: 10,
-  };
-  
-  var purchases = [
-    {
-      item: 'Carrot',
-      units: 20,
-    },
-    {
-      item: 'Apple',
-      units: 2,
-    },
-    {
-      item: 'Guava',
-      units: 1,
-    },
-  ];
-  
-  /* Functions */
-  const getDiscountPercent = (productName) => {
-    let discount=discounts[productName];
-    let discountPercent=(discount>0)?(discount/100):0;
-    return discountPercent;
-  };
-  
-  const getTaxPercent =  (productName) => {
-    let tax=taxes[productName];
-    let taxPercent=(tax>0)?(tax/100):0;
-    return taxPercent;
-  };
-  
-  const getUnitPrice = (itemName) => {
-    let unitPrice=rates[itemName];
-    let discountPercent=getDiscountPercent(itemName);
-    let itemDiscountedPrice=unitPrice*discountPercent;
-    let discountedPrice=unitPrice-itemDiscountedPrice;
-    let taxPercent=getTaxPercent(itemName);
-    let taxPrice=discountedPrice*taxPercent;
-    let finalPrice=discountedPrice+taxPrice;
-    return finalPrice;
-  };
-  
-  const getLineItemPrice = (lineItem) => {
-    let itemName=lineItem['item'];
-    let unit=lineItem['units'];
-    let unitPrice=getUnitPrice(itemName);
-    let totalUnitsPrice=unitPrice*unit;
-    let itemPrice={
-        'price': totalUnitsPrice,
-        'item': itemName,    
-    };
-    return itemPrice;
-  };
-  
-  const getSum = function () {
-    lineItemPrice=purchases.map(getLineItemPrice)
-    let sum=0;
-    let itemLength=lineItemPrice.length;
-    for(let i=0;i<itemLength;i++){
-      sum=sum+lineItemPrice[i].price;
-    }
-    console.log("Total Sum : "+sum);
-      console.table(lineItemPrice);
-  };
-  
-  // Do not change below this line.
-  /* Main Function */
-  const main = function main() {
-    getSum();
-  }
-  main();
+  return itemPrice;
+};
+
+const getSum = () => {
+  let lineItemPrice = purchases.map(getLineItemPrice)
+  let sum = 0;
+  sum=(lineItemPrice.map(item=>item.price)).reduce((total,price)=>total+price);
+  console.log("Total Sum : " + sum);
+  console.table(lineItemPrice);
+};
+
+// Do not change below this line.
+/* Main Function */
+const main = () => {
+  getSum();
+}
+main();
